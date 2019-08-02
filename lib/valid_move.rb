dc9f7de641eb1fb0303144fb2f494a6c2e79e693
Abdullah Ahmed <abdullah@softcomnetworks.com>
2019-08-02 11:33:50 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,7 +2,7 @@
 def valid_move?(board, index)
   if !(index.between?(1,8))
     return false
-  elsif position_taken(board, index) == true
+  elsif position_taken?(board, index) == true
     return false
   else
     return true
